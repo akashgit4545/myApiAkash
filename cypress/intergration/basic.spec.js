6d4049ebaaaa8e46d0e02ad8062e965ba5cb5d92
akkigit4545 <akkipatil002325@gmail.com>
2025-03-01 19:30:47 +0530

    second commit

--- cypress/intergration/basic.spec.js
+++ cypress/intergration/basic.spec.js
@@ -1,6 +1,10 @@
 describe("start with basic cypress commands", () => {
   it("start cypress", () => {
     cy.visit("https://www.google.com/");
-    cy.url().should('eq','https://www.google.com/');
+    cy.url().should("eq", "https://www.google.com/");
+  });
+
+  it("second url to viit", () => {
+    cy.visit("https://www.youtube.com/");
   });
 });
